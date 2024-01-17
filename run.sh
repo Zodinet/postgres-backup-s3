210b97335500ee6c7d73353c5aca87a0be61a0e3
Hồ Duy Nhật Linh <linh232324@gmail.com>
2024-01-17 14:09:09 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
 
-set -eu
+set -eo pipefail
 
-if [ "$S3_S3V4" = "yes" ]; then
-  aws configure set default.s3.signature_version s3v4
+if [ "${S3_S3V4}" = "yes" ]; then
+    aws configure set default.s3.signature_version s3v4
 fi
 
-if [ -z "$SCHEDULE" ]; then
+if [ "${SCHEDULE}" = "**None**" ]; then
   sh backup.sh
 else
-  exec go-cron "$SCHEDULE" /bin/sh backup.sh
+  echo -e "SHELL=/bin/sh\n${SCHEDULE} /bin/sh /backup.sh" > /etc/crontabs/root
+  exec go-crond /etc/crontabs/root
 fi
